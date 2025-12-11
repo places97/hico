512e8136bc95a8a119879308313e0eaec8e7ae31
hayei <hayei@suh>
2025-12-11 17:57:14 +0900

    java 파일에

--- src/main/java/kr/go/hico/cmm/cmm/web/HomeController.java
+++ src/main/java/kr/go/hico/cmm/cmm/web/HomeController.java
@@ -4,17 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class HomeController {
+	
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class)
 
   @RequestMapping("/")
   public String root() {
+	  log.trace("spring init trace");
+	  log.debug("spring init debug");
+	  log.info("spring init info");
+	  log.warn("spring init warn");
+	  log.error("spring init error");
+	  
     return "redirect:/home.do";
   }
 
   @GetMapping("/home.do")
   public String index(Model model) {
+	  log.info("home page init");
     model.addAttribute("contentTitle", "메인 화면");
     return "index";
   }
