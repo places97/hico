4bb8877bbd0d9063a9a566e8d9bb90a1baa681e8
LeeGaEun <dmsalsgml84@gmail.com>
2025-12-11 15:20:29 +0900

    rename: 파일명 변경

--- src/main/java/kr/go/hico/cmm/cmm/web/HomeCtl.java
+++ src/main/java/kr/go/hico/cmm/cmm/web/HomeCtl.java
@@ -1,12 +1,14 @@
 package kr.go.hico.cmm.cmm.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
-public class HomeController {
+public class HomeCtl {
 
   @RequestMapping("/")
   public String root() {
